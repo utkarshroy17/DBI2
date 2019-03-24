e1b0fd722502c4fbff2e0d5f39718b9b135b3072
Utkarsh Roy <uroy@DESKTOP-5F22JG0.localdomain>
2019-03-23 20:31:51 -0400

    Added select pipe

--- RelOp.cc
+++ RelOp.cc
@@ -2,9 +2,10 @@
 #include <pthread.h>
 #include <iostream>
 
-void *AddToOut(void *args) {
+// Select file implementation
+void *SelectFile::ReadFromDBFile(void *args) {
 	
-	sf_utils *sf = (sf_utils *)args;
+	thread_utils *sf = (thread_utils *)args;
 	ComparisonEngine ceng;
 	Record temp;
 	Schema *testSchema = new Schema("catalog", "partsupp");
@@ -31,15 +32,15 @@ void SelectFile::Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal
 	
 	selOp.Print();
 
-	//sf_utils args = { inFile, &outPipe, selOp, literal };
-	sf_utils *args = new sf_utils;
+	//thread_utils args = { inFile, &outPipe, selOp, literal };
+	thread_utils *args = new thread_utils;
 
 	args->inFile = inFile;
 	args->outPipe = &outPipe;
 	args->literal = literal;
 	args->selOperator = selOp;
 
-	pthread_create(&thread, NULL, AddToOut, (void *) args);
+	pthread_create(&thread, NULL, ReadFromDBFile, (void *) args);
 }
 
 void SelectFile::WaitUntilDone () {
@@ -48,5 +49,76 @@ void SelectFile::WaitUntilDone () {
 }
 
 void SelectFile::Use_n_Pages (int runlen) {
+	runLength = runlen;
+}
+
+// Select Pipe implementation
+void *SelectPipe::ReadFromPipe(void *args) {
+
+	thread_utils *sf = (thread_utils *)args;
+	ComparisonEngine ceng;
+	Record temp;
+	Schema *testSchema = new Schema("catalog", "partsupp");
+
+	sf->selOperator.Print();
+
+	while (sf->inPipe->Remove(&temp)) {
+
+		if (ceng.Compare(&temp, &sf->literal, &sf->selOperator)) {
+			sf->outPipe->Insert(&temp);
+		}
+	}
+
+	sf->outPipe->ShutDown();
+}
+
+
+void SelectPipe::Run(Pipe &inPipe, Pipe &outPipe, CNF &selOp, Record &literal) {
+
+	selOp.Print();
+
+	//thread_utils args = { inFile, &outPipe, selOp, literal };
+	thread_utils *args = new thread_utils;
+
+	args->inPipe = &inPipe;
+	args->outPipe = &outPipe;
+	args->literal = literal;
+	args->selOperator = selOp;
+
+	pthread_create(&thread, NULL, ReadFromPipe, (void *)args);
+}
+
+void SelectPipe::WaitUntilDone() {
+
+	pthread_join(thread, NULL);
+}
+
+void SelectPipe::Use_n_Pages(int runlen) {
+	runLength = runlen;
+}
+
+// Sum implementation
+void *Sum::ComputeSum(void *args)
+{
+	thread_utils *s = new thread_utils;
+
+}
+
+void Sum::Run(Pipe &inPipe, Pipe &outPipe, Function &computeMe) {
+
+	thread_utils *args = new thread_utils;
+
+	args->inPipe = &inPipe;
+	args->outPipe = &outPipe;
+	args->computeMe = computeMe;
+
+	pthread_create(&thread, NULL, ComputeSum, (void *)args);
+}
+
+void Sum::WaitUntilDone() {
+	pthread_join(thread, NULL);
+}
+
+void Sum::Use_n_Pages(int runlen) {
 	runLength = runlen;
 }
